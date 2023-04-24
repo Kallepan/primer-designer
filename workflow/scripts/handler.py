e449b907424f540aef7e66c6a028f5d4e04cdb43
Kajeepan Kandeepan <kajeepan.kandeepan@gmail.com>
2023-04-24 15:01:50 +0000

    reimplemented script for generating proto primers

---
+++ workflow/scripts/handler.py
@@ -0,0 +1,95 @@
+import re
+import subprocess
+import os
+from Bio.Seq import Seq 
+
+class PrimerGenerator():
+    """
+    Generates the Primers for a given amplicon using Primer3
+    """
+
+    def __init__(self, region_name: str, amplicon_index: int, amplicon_sequence: Seq, amplicon_overlap: int, primer3_settings: str, temp_dir: str, iteration_index: int):
+        self._primer3_settings = primer3_settings
+        self.amplicon_sequence = amplicon_sequence
+        self.amplicon_overlap = amplicon_overlap
+        self.amplicon_id = f"{region_name}-{amplicon_index}-{iteration_index}"
+        self.temp_dir = temp_dir
+
+    def run(self) -> tuple[list, list]:
+        """ 
+        Generate the input file for primer3 and write it to a temporary file
+        Take the output from primer 3 and parse it to this classes parsers
+        Return the primer pairs along with additional information
+        or None if either no forward or reverse primers
+        """
+        file_name = self.__write_temp_primer_gen_file()
+        
+        # Run primer3_core
+        command = f"primer3_core < {file_name}"
+        result = subprocess.run(
+            command, 
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        
+        stdout, stderr = result.stdout, result.stderr
+        
+        if result.returncode != 0:
+            raise Exception(f"Primer3 failed with error: {stderr}")
+
+        return self.__parse_output_from_primer3(stdout) 
+    
+    def __write_temp_primer_gen_file(self) -> str:
+        PRIMER3_OK_REGION_LIST=f"0,{self.amplicon_overlap},{len(self.amplicon_sequence) - self.amplicon_overlap},{self.amplicon_overlap}"
+        with open(os.path.join(self.temp_dir, self.amplicon_id), "w") as file:
+            file.write(f"SEQUENCE_PRIMER_PAIR_OK_REGION_LIST={PRIMER3_OK_REGION_LIST}\n")
+            file.write(f"SEQUENCE_ID={self.amplicon_id}\n")
+            file.write(f"SEQUENCE_TEMPLATE={str(self.amplicon_sequence)}\n")
+            file.write(self._primer3_settings)
+            
+            return file.name
+
+    def __extract_primer_data(self, n_primers: int, output: str, left_or_right: str) -> list:
+        primers = []
+        for i in range(1, n_primers):
+            pattern = rf"PRIMER_{left_or_right}_{i}_(SEQUENCE|TM|GC_PERCENT|HAIRPIN_TH)=(\w+.*)\r?\n"
+            data_pattern = re.compile(pattern)
+            data = re.findall(data_pattern, output)
+            primer_data = {}
+            for entry in data:
+                key = entry[0].lower()
+                try:
+                    value = float(entry[1])
+                except ValueError:
+                    value = entry[1]
+                
+                primer_data[key] = value
+            primer_data["length"] = len(primer_data["sequence"])
+            primers.append(primer_data)
+        return primers
+    
+    def __parse_output_from_primer3(self, output: bytes) -> tuple[list, list]:
+        # convert byte output to string output, afterwards find the desired sequences
+        pattern = re.compile(r"PRIMER_(LEFT|RIGHT)_NUM_RETURNED=(\d+)\r?\n")
+
+        output = str(output, "utf-8")
+        pattern_search_result = re.findall(pattern, output)
+
+        if pattern_search_result is None or len(pattern_search_result) != 2:
+            raise Exception("Primer3 failed to return any primers")
+        
+        n_left_primers = int(pattern_search_result[0][1])
+        n_right_primers = int(pattern_search_result[1][1])
+
+        print(f"Found {n_left_primers} left primers and {n_right_primers} right primers for sequence {self.amplicon_id}")
+
+        """
+        With the number of primers returned we iterate over the output
+        and extract the data for each primerpair. The data is stored in a dictionary
+        corresponding to the left and right primer. This is then stored in the overall results dictionary
+        """
+
+        forward_primers = self.__extract_primer_data(n_left_primers, output, "LEFT")
+        reverse_primers = self.__extract_primer_data(n_right_primers, output, "RIGHT")
+        
+        return forward_primers, reverse_primers
---
+++ workflow/scripts/proto_primers.py
@@ -0,0 +1,287 @@
+import os
+import sys
+import argparse
+import yaml
+
+from Bio import SeqIO
+from Bio.SeqIO import SeqRecord
+from Bio.Seq import Seq
+
+import pandas as pd
+
+from handler import PrimerGenerator
+
+DEFAULT_AMPLICON_SIZE=200
+DEFAULT_MIN_AMPLICON_OVERLAP=50
+DEFAULT_MAX_AMPLICON_OVERLAP=150
+DEFAULT_OVERLAP_INCREASE=10
+DEFAULT_OVERLAP_SHIFT=10
+
+def __load_primer3_settings(path: str) -> str:
+    """
+    Load the primer3 settings from the config file
+    """
+    KEYS_TO_IGNORE = ["PRIMER_PRODUCT_SIZE_RANGE", "SEQUENCE_PRIMER_PAIR_OK_REGION_LIST"]
+    settings = {}
+
+    with open(path, "r") as handle:
+        settings = yaml.safe_load(handle)
+    
+    settings_str = ""
+
+    for k in KEYS_TO_IGNORE:
+        settings.pop(k, None)
+
+    for k, v in settings.items():
+        settings_str += f"{k}={v}\n"
+
+    settings_str += "="
+
+    return settings_str 
+
+def __load_regions(path: str) -> pd.DataFrame:
+    df = pd.read_csv(
+        path,
+        sep=",",
+        header=0,
+        names=["loci", "start", "end"],
+        dtype={"loci": str, "start": int, "end": int},
+    )
+
+    return df
+
+def __extract_sequence_record_from_fasta(path: str) -> SeqRecord:
+    with open(path, "r") as file:
+        seq = list(SeqIO.parse(file, "fasta"))
+    
+    if len(seq) == 0:
+        raise Exception("No sequences found in fasta file")
+    
+    if len(seq) > 1:
+        raise Exception("More than one sequence found in fasta file")
+    
+    return seq[0]
+
+def get_parser() -> argparse.ArgumentParser:
+    """
+        Returns the parser for the script.
+    """
+
+    parser = argparse.ArgumentParser(
+        description=
+        """
+        Generate a list of amplicons and generate Proto Primers for each amplicon from a sequence in a fasta file.
+        Save the list of amplicons to a fasta file as well as the Proto Primers to a json.
+        """
+    )
+    parser.add_argument(
+        "-f", 
+        "--fasta",
+        type=str,
+        required=True,
+        help="Path to the fasta file containing the sequence to be used to generate the amplicons."
+    )
+    parser.add_argument(
+        "-c",
+        "--config_file",
+        type=str,
+        required=True,
+        help="Path to the primer3 config file."
+    )
+    parser.add_argument(
+        "-r",
+        "--regions",
+        type=str,
+        required=True,
+        help="Path to the csv file containing the regions to be used to generate the amplicons."
+    )
+    parser.add_argument(
+        "-o",
+        "--output_dir",
+        type=str,
+        required=True,
+        help="Path to the output directory where the amplicons and proto primers will be saved."
+    )
+    parser.add_argument(
+        "-t",
+        "--temp_dir",
+        type=str,
+        help="Path to the temporary directory to be used when generating the amplicons."
+    )
+    parser.add_argument(
+        "--amplicon_size",
+        type=int,
+        default=DEFAULT_AMPLICON_SIZE,
+        help=f"The size of the amplicons to be generated. Default is {DEFAULT_AMPLICON_SIZE}."
+    )
+    parser.add_argument(
+        "--min_overlap",
+        type=int,
+        default=DEFAULT_MIN_AMPLICON_OVERLAP,
+        help=f"The minimum overlap to be used when generating the amplicons. Default is {DEFAULT_MIN_AMPLICON_OVERLAP}."
+    )
+    parser.add_argument(
+        "--max_overlap",
+        type=int,
+        default=DEFAULT_MAX_AMPLICON_OVERLAP,
+        help=f"The maximum overlap to be used when generating the amplicons. Default is {DEFAULT_MAX_AMPLICON_OVERLAP}."
+    )
+    parser.add_argument(
+        "--overlap_increase",
+        type=int,
+        default=DEFAULT_OVERLAP_INCREASE,
+        help="The amount by which the overlap will be increased when generating the amplicons and if the previous overlap returned no primer pairs."
+    )
+    parser.add_argument(
+        "--overlap_shift",
+        type=int,
+        default=DEFAULT_OVERLAP_SHIFT,
+        help="The amount by which the overlap will be shifted when generating the amplicons and if the previous overlap returned no primer pairs."
+    )
+    return parser
+
+def __generate_amplicon_and_primers_by_regions(
+        region_name: str,
+        region_start: int,
+        region_end: int,
+        sequence: Seq,
+        amplicon_size: int,
+        amplicon_max_overlap: int,
+        amplicon_min_overlap: int,
+        overlap_increase_increment: int,
+        overlap_shift_increment: int,
+        primer3_settings_path: str,
+        temp_dir: str
+    ) -> list:
+    """
+        Split a given region into amplicons by including a certain overlap in both directions.
+    """
+    primer3_settings = __load_primer3_settings(primer3_settings_path)
+
+    amplicons = []
+
+    amplicon_size = amplicon_size - (2 * amplicon_min_overlap)
+    amplicon_offset = - amplicon_min_overlap
+    index = 0
+    while amplicon_offset < (region_end - region_start + amplicon_min_overlap):
+        # Executed for each amplicon
+        amplicon_overlap = amplicon_min_overlap
+        amplicon_shift = 0
+        primer_generation_iteration_index = 0
+        amplicon_forward_primers = []
+        amplicon_reverse_primers = []
+
+        while True:
+            # Shifting strategy primer generation
+            primer_generation_iteration_index += 1
+            amplicon_start  = amplicon_offset + amplicon_shift - amplicon_overlap
+            amplicon_end    = amplicon_offset + amplicon_shift + amplicon_overlap + amplicon_size  
+
+            if amplicon_shift >= amplicon_overlap:
+                break
+            
+            amplicon_sequence = sequence[amplicon_start + region_start: amplicon_end + region_start]
+
+            primer_generator = PrimerGenerator(
+                region_name=region_name,
+                amplicon_index=index,
+                amplicon_sequence=amplicon_sequence,
+                primer3_settings=primer3_settings,
+                temp_dir=temp_dir,
+                amplicon_overlap=amplicon_overlap,
+                iteration_index=primer_generation_iteration_index
+            )
+
+            forward_primers, reverse_primers = primer_generator.run()
+            # Append non-empty forward_primers and reverse_primers to the amplicons dict
+            # if either is not empty and the list does not have entries break this loop
+            
+            amplicon_forward_primers.extend(forward_primers)
+            amplicon_reverse_primers.extend(reverse_primers)
+
+            if forward_primers and reverse_primers:
+                break
+
+            amplicon_shift += overlap_shift_increment
+            continue
+
+        # TODO: Add additional stragies for when no primers are found
+
+        if not amplicon_forward_primers and not amplicon_reverse_primers:
+            print(f"Failed to find primers for {region_name}-{index} in region {amplicon_offset}-{amplicon_offset + amplicon_size + amplicon_overlap}")
+
+        # clean forward and reverse primers (e.g.: remove duplicates)
+
+        amplicon = {
+            "name": f"{region_name}-{index}",
+            "forward_primers": amplicon_forward_primers,
+            "reverse_primers": amplicon_reverse_primers,
+        }
+        amplicons.append(amplicon)
+        amplicon_offset += amplicon_size
+        index += 1
+
+    return amplicons
+
+def main():
+    args = get_parser().parse_args()
+
+    # Error Handling
+    if not os.path.exists(args.fasta):
+        raise Exception(f"The fasta file {args.fasta} does not exist.")
+    if not os.path.exists(args.regions):
+        raise Exception(f"The regions file {args.regions} does not exist.")
+    if not os.path.exists(args.config_file):
+        raise Exception(f"The primer3 config file {args.config_file} does not exist.")
+    if not os.path.exists(args.output_dir):
+        raise Exception(f"The output directory {args.output} does not exist.")
+    if not os.path.exists(args.temp_dir):
+        os.mkdir(args.temp_dir)
+
+    if args.min_overlap > args.max_overlap:
+        raise Exception("The minimum overlap cannot be greater than the maximum overlap.")
+    if args.min_overlap < 0:
+        raise Exception("The minimum overlap cannot be less than 0.")
+    if args.max_overlap < 0:
+        raise Exception("The maximum overlap cannot be less than 0.")
+    if args.overlap_increase < 0:
+        raise Exception("The overlap increase cannot be less than 0.")
+    if args.overlap_shift < 0:
+        raise Exception("The overlap shift cannot be less than 0.")
+
+    if args.amplicon_size < 0:
+        raise Exception("The amplicon size cannot be less than 0.")
+    if args.min_overlap > args.amplicon_size:
+        raise Exception("The overlap cannot be greater than the amplicon size.")
+    if args.max_overlap > args.amplicon_size:
+        raise Exception("The overlap cannot be greater than the amplicon size.")
+    
+    # Load the regions
+    regions = __load_regions(args.regions)
+
+    # Load the sequence
+    seq_record = __extract_sequence_record_from_fasta(args.fasta)
+
+    # Generate the amplicons by each regions
+    amplicons_by_regions = {}
+    for i, row in regions.iterrows():
+        __generate_amplicon_and_primers_by_regions(
+            region_name=row["loci"],
+            region_start=row["start"],
+            region_end=row["end"],
+            amplicon_min_overlap=args.min_overlap,
+            amplicon_max_overlap=args.max_overlap,
+            amplicon_size=args.amplicon_size,
+            overlap_increase_increment=args.overlap_increase,
+            overlap_shift_increment=args.overlap_shift,
+            primer3_settings_path=args.config_file,
+            temp_dir=args.temp_dir,
+            sequence=seq_record.seq
+        )
+
+if __name__ == "__main__":
+    #try:
+        main()
+    #except Exception as e:
+    #    sys.stderr.write(f"ERROR: {e}\n")
+    #    sys.exit(1)
