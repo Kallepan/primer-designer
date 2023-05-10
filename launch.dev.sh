e2a1ed4d5315f4a7880f0900ee3b5cfbfcab15de
Kajeepan Kandeepan <kajeepan.kandeepan@gmail.com>
2023-05-10 14:58:10 +0000

    snakemake primer filtering

--- launch.dev.sh
+++ launch.dev.sh
@@ -8,3 +8,4 @@ rm logs/*
 # python3 workflow/scripts/proto_primers.py -f data/myc_tuberculosis_h37Rv.fasta -p config/primer3_settings.yaml -r data/loci_formatted.csv -t tmp/ -o results/test.json
 
 snakemake --use-conda -p results/myc_tuberculosis_h37Rv.proto_primers.json --cores 2
+snakemake --use-conda -p results/myc_tuberculosis_h37Rv.proto_primers.1.fasta --cores 2
---
+++ workflow/scripts/format_into_fasta.py
@@ -0,0 +1,61 @@
+import json
+import argparse
+
+import os
+from io import TextIOWrapper
+
+def __parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, required=True)
+    parser.add_argument('--output', type=str, required=True)
+    return parser.parse_args()
+
+def __load_json(path: str):
+    with open(path, 'r') as f:
+        return json.load(f)
+
+def __get_sequence_from_primer(primer: dict) -> str:
+    return primer.get('primer_sequence', "")
+
+def __extract_primer(primers: list[dict], file: TextIOWrapper, region_name: str, amplicon_name: str) -> None:
+    if not primers:
+        return None
+
+    # do not add primers if there is only one
+    if len(primers) == 1:
+        return None
+
+    for i, primer in enumerate(primers):
+        sequence = __get_sequence_from_primer(primer)
+        name_str = f">{region_name}|{amplicon_name}|forward|{i}\n"
+        file.write(name_str)
+        file.write(sequence)
+        file.write("\n")
+
+def __write_fasta(data: dict, file: TextIOWrapper):
+    for region in data['regions']:
+        region_name = region['region_name']
+
+        for amplicon in region['amplicons']:
+            amplicon_name = amplicon['amplicon_name']
+
+            # Extract the sequence frome the primer and write to file
+            # Name Encoding: region_name|amplicon_name|(forward || reverse)|index
+            __extract_primer(amplicon["forward_primers"], file, region_name, amplicon_name)
+            __extract_primer(amplicon["reverse_primers"], file, region_name, amplicon_name)
+
+def main():
+    args = __parse_args()
+    data = __load_json(args.input)
+    try:
+        file = open(args.output, 'w')
+        __write_fasta(data, file)
+    except Exception as e:
+        print(f"Error writing to file {args.output}")
+        print(e)
+    finally:
+        file.close()
+
+
+if __name__ == "__main__":
+    main()
