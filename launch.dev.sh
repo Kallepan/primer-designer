1266c8303ebaaf498584921817d9615e9d32e23f
Kajeepan Kandeepan <kajeepan.kandeepan@gmail.com>
2023-07-16 20:06:19 +0000

    cleanup: dev files

---
+++ launch.dev.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# This script is used to debug various parts of the pipeline.
+# Its content should not be considered as part of the pipeline
+# and should not be used as a reference for how to run the pipeline!
+
+# main pipeline
+rm /homes/kkandeepan/data/results/* -rf
+rm results -rf
+rm logs -rf
+rm tmp -rf
+
+snakemake --use-conda --use-singularity -j 4 -p results/myc_tuberculosis_h37Rv.dummy
+
+# development
+rm /homes/kkandeepan/data/results/* -rf
+rm results -rf
+rm logs -rf
+rm tmp -rf
+
+snakemake --use-conda --use-singularity -j 5 -p results/myc_tuberculosis_h37Rv.GRCh38.0.eval
+python3 workflow/scripts/eval_primers_foreign.py --db results/myc_tuberculosis_h37Rv.db --pool 0 --output results/filter/foreign/test_foreign.tsv --species GRCh38
